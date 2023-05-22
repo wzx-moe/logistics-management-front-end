e701b68370abcb8d242dfb6824499b4b337e6a35
wzx <wzx.moe@gmail.com>
2023-05-22 18:18:14 +1000

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./Login.js"
 import Customer from "./Customer";
